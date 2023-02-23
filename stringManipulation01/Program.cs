0494056b5278c0979ed054484259456a1db5ffa5
DarshanBorse333 <Darshanborse@gmail.com>
2023-02-23 18:26:20 +0530

    first string program

--- stringManipulation01/Program.cs
+++ stringManipulation01/Program.cs
@@ -5,6 +5,21 @@ public class Program
 {
     public static void Main()
     {
-        Console.WriteLine("Enter any string ");
+
+        string fName, mName, lName, fullName; 
+        Console.WriteLine("Enter your First Name ");
+        fName = Console.ReadLine();
+
+        Console.WriteLine("Enter your Middle Name ");
+        mName = Console.ReadLine();
+
+        Console.WriteLine("Enter your Last Name ");
+        lName = Console.ReadLine();
+
+        fullName = fName + " " + mName + " " + lName;
+        Console.WriteLine(fullName);
+
+        var str = Console.ReadLine();
+        Console.WriteLine(str);
     }
 }

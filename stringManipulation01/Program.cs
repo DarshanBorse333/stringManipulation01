c3132beb438800bd2f40264cade1d5db44a8dfea
DarshanBorse333 <Darshanborse@gmail.com>
2023-02-23 20:25:46 +0530

    Some another

--- stringManipulation01/Program.cs
+++ stringManipulation01/Program.cs
@@ -6,20 +6,49 @@ public class Program
     public static void Main()
     {
 
-        string fName, mName, lName, fullName; 
-        Console.WriteLine("Enter your First Name ");
-        fName = Console.ReadLine();
+        //string fName, mName, lName, fullName; 
+        //Console.WriteLine("Enter your First Name ");
+        //fName = Console.ReadLine();
 
-        Console.WriteLine("Enter your Middle Name ");
-        mName = Console.ReadLine();
+        //Console.WriteLine("Enter your Middle Name ");
+        //mName = Console.ReadLine();
 
-        Console.WriteLine("Enter your Last Name ");
-        lName = Console.ReadLine();
+        //Console.WriteLine("Enter your Last Name ");
+        //lName = Console.ReadLine();
 
-        fullName = fName + " " + mName + " " + lName;
-        Console.WriteLine(fullName);
+        //fullName = fName + " " + mName + " " + lName;
+        //Console.WriteLine(fullName);
 
+        //var str = Console.ReadLine();
+        //Console.WriteLine(str); 
+
+        //Console.WriteLine("Enter any string :");
+        //var str = Console.ReadLine();
+
+        //int counter = 0;
+
+        //foreach (var item in str)
+        //{
+        //    counter++;
+        //}
+
+        //Console.WriteLine("length of character is = "+counter);
+
+        Console.WriteLine("Please Enter any string : ");
         var str = Console.ReadLine();
-        Console.WriteLine(str);
+
+        int vowelsCounter = 0;
+
+        foreach (var item in str)
+        {
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
+                item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+            {
+                vowelsCounter++;
+            }
+
+        }
+            Console.WriteLine("total number of vowels are : " + vowelsCounter);
+        
     }
 }

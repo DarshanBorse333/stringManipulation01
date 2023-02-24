dcb7b8e629c8a8c51b78d60c2646d859e456ee95
DarshanBorse333 <Darshanborse@gmail.com>
2023-02-24 17:55:06 +0530

    string manipulation

--- stringManipulation01/Program.cs
+++ stringManipulation01/Program.cs
@@ -6,97 +6,105 @@ public class Program
     public static void Main()
     {
 
-        //string fName, mName, lName, fullName; 
-        //Console.WriteLine("Enter your First Name ");
-        //fName = Console.ReadLine();
+        StringManipulation();//function call // this can be called by it's class name,Program.StringManipulation()
 
-        //Console.WriteLine("Enter your Middle Name ");
-        //mName = Console.ReadLine();
+        ShowFullName();  //function call   // but no need to write class name, bcz it's in same code
 
-        //Console.WriteLine("Enter your Last Name ");
-        //lName = Console.ReadLine();
+        CalculateLengthOfString();
 
-        //fullName = fName + " " + mName + " " + lName;
-        //Console.WriteLine(fullName);
-
-
-
-
-        //Calculate lenght of the string//
-
-        //Console.WriteLine("Enter any string :");
-        //var str = Console.ReadLine();
-
-        //int counter = 0;
-
-        //foreach (var item in str)
-        //{
-        //    counter++;
-        //}
-
-        //Console.WriteLine("length of character is = "+counter);
-
-
-
-
-        //caluculate the vowels from the string//
-
-        //Console.WriteLine("Please Enter any string : ");
-        //var str = Console.ReadLine();
-
-        //int vowelsCounter = 0;
-
-        //foreach (var item in str)
-        //{
-        //    if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
-        //        item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
-        //    {
-        //        vowelsCounter++;
-        //    }
-
-        //}
-        //    Console.WriteLine("total number of vowels are : " + vowelsCounter);
-
-
-
-
-        // Calculate spaces between string //
-
-        //Console.WriteLine("Enter any Line of string : ");
-        //var str = Console.ReadLine();
-
-        //int spacecounter = 0;
-
-        //foreach (var item in str)
-        //{
-        //    if (item == ' ')
-        //    {
-        //        spacecounter++;
-        //    }
-        //}
-
-        //Console.WriteLine("number of spaces are = " + spacecounter);
-
-
-
-
-        //calculate consonents from the string //
-
-        Console.WriteLine("Enter any line of string :");
-        var str = Console.ReadLine();
-
-        int ConsonentCount = 0;
-
-        foreach(var item in str)
-        {
-            if(item != 'a' && item != 'e' && item != 'i' && item != 'o' && item != 'u' &&
-                item != ' ' && item != 'A' && item != 'E' && item != 'I' && item != 'O' && item != 'U')
-            {
-                ConsonentCount++;
-            }
-        }
-        Console.WriteLine("Consonent count is = " + ConsonentCount);
+        CalculateVowels();
 
 
     }
+
+    //stringManipulationOfAnything  camelCase
+    //StringManipulationOfAnything  PascalCase
+
+    private static void StringManipulation()
+    {
+        
+        Console.WriteLine("Enter any string");
+        var str = Console.ReadLine();          //this is function call,& Console is class name&ReadLine is function name
+
+        int spaceCounter = 0;
+        int consonantCounter = 0;
+
+        foreach (char ch in str)
+        {
+            if(ch == ' ')
+            {
+                spaceCounter++;
+            }
+
+            if(   !(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' ||
+                ch == ' ' || ch == 'A' || ch == 'E' || ch == 'I' || ch == 'O' || ch == 'U'))
+            {
+                consonantCounter++;
+            }
+        }
+
+        Console.WriteLine("Number of spaces = " + spaceCounter);
+        Console.WriteLine("Number of words = " + (spaceCounter + 1));
+        Console.WriteLine("Number of consonants = " + consonantCounter);
+    }
+
+
+    private static void ShowFullName()
+    {   
+        // Show the full Name //
+
+        string fName, mName, lName, fullName; 
+        Console.WriteLine("Enter your First Name ");
+        fName = Console.ReadLine();
+
+        Console.WriteLine("Enter your Middle Name ");
+        mName = Console.ReadLine();
+
+        Console.WriteLine("Enter your Last Name ");
+        lName = Console.ReadLine();
+
+        fullName = fName + " " + mName + " " + lName;
+        Console.WriteLine(fullName);
+    }
+
+
+    private static void CalculateLengthOfString()
+    {
+        //Calculate lenght of the string//
+
+        Console.WriteLine("Enter any string :");
+        var str = Console.ReadLine();
+
+        int counter = 0;
+
+        foreach (var item in str)
+        {
+            counter++;
+        }
+
+        Console.WriteLine("length of character is = " + counter);
+    }
+
+
+    private static void CalculateVowels()
+    {
+        //caluculate the vowels from the string//
+
+        Console.WriteLine("Please Enter any string : ");
+        var str = Console.ReadLine();
+
+        int vowelsCounter = 0;
+
+        foreach (var item in str)
+        {
+            if (item == 'a' || item == 'e' || item == 'i' || item == 'o' || item == 'u' ||
+                item == 'A' || item == 'E' || item == 'I' || item == 'O' || item == 'U')
+            {
+                vowelsCounter++;
+            }
+
+        }
+            Console.WriteLine("total number of vowels are : " + vowelsCounter);
+    }
 }
+
